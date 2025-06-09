a0742301d27ffe3a7ea6cdc1b665366942b03376
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2025-06-09 20:25:04 +0530

    Time: 0 ms (100%), Space: 8 MB (9.23%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,30 +1,30 @@
 class Solution {
- public:
-  int findKthNumber(int n, int k) {
-    long ans = 1;
+public:
+    int findKthNumber(int n, int k) {
+        long long res = 1;
 
-    for (int i = 1; i < k;) {
-      const long gap = getGap(ans, ans + 1, n);
-      if (i + gap <= k) {
-        i += gap;
-        ++ans;
-      } else {
-        ++i;
-        ans *= 10;
-      }
+        for (int i = 1; i < k;) {
+            long long gap = getGap(res, res + 1, n);
+            if (i + gap <= k) {
+                i += gap;
+                ++res;
+            } else {
+                ++i;
+                res *= 10;
+            }
+        }
+
+        return static_cast<int>(res);
     }
 
-    return ans;
-  }
-
- private:
-  long getGap(long a, long b, long n) {
-    long gap = 0;
-    while (a <= n) {
-      gap += min(n + 1, b) - a;
-      a *= 10;
-      b *= 10;
+private:
+    long long getGap(long long a, long long b, int n) {
+        long long gap = 0;
+        while (a <= n) {
+            gap += std::min(static_cast<long long>(n) + 1, b) - a;
+            a *= 10;
+            b *= 10;
+        }
+        return gap;
     }
-    return gap;
-  };
 };
