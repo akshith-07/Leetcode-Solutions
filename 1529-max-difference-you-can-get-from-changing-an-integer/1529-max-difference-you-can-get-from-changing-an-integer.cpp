22ea8fae23bfbdcf524f2bb6f5a3ef8ad714d4b2
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2025-06-15 17:58:42 +0530

    Time: 3 ms (15.17%), Space: 9.1 MB (18.01%) - LeetHub

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDiff(int num) {
+        string numStr = to_string(num);
+        set<char> uniqueDigits(numStr.begin(), numStr.end());
+        int maxVal = num, minVal = num;
+
+        for (char digit : uniqueDigits) {
+            for (char newDigit = '0'; newDigit <= '9'; ++newDigit) {
+                if (numStr[0] == digit && newDigit == '0') continue;
+                string newNumStr = numStr;
+                replace(newNumStr.begin(), newNumStr.end(), digit, newDigit);
+                int newNum = stoi(newNumStr);
+                maxVal = max(maxVal, newNum);
+                minVal = min(minVal, newNum);
+            }
+        }
+
+        return maxVal - minVal;
+    }
+};
