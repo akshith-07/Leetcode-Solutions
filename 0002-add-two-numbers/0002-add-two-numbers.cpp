0ef8668d69f323627925b27842262132de2c129b
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2026-09-19 13:11:20 +0530

    Time: 30 ms (7.04%), Space: 77.1 MB (75.69%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,37 +11,43 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
-        ListNode* dummy=new ListNode();
-        ListNode * temp=dummy;
-
-        int carry=0;
-
-        while(l1!=NULL || l2!=NULL || carry)
-        {
-            int sum=0;
-
-            if(l1!=NULL)
-            {
-                sum+=l1->val;
-                l1=l1->next;
+        ListNode* output = new ListNode(0);
+        ListNode* dummy = output;
+        int carry =0;
+        ListNode* newNode;
+        while(l1!=nullptr || l2!=nullptr){
+            if(l1!=nullptr && l2!=nullptr){
+                carry = l1->val + l2->val + carry;
+                newNode = new ListNode(carry%10);
+                cout<<newNode->val<<" ";
+                carry = carry/10;
+            }else if(l1==nullptr && l2!=nullptr){
+                carry = l2->val + carry;
+                newNode = new ListNode(carry%10);
+                cout<<newNode->val<<" ";
+                carry = carry/10;
+            }else if(l1!=nullptr && l2==nullptr){
+                carry = l1->val + carry;
+                newNode = new ListNode(carry%10);
+                cout<<newNode->val<<" ";
+                carry = carry/10;
             }
+        
+            output->next = newNode;
+            output=output->next;
+           
+            if(l1!=nullptr)l1=l1->next;
+            if(l2!=nullptr)l2=l2->next;
+        }
 
-            if(l2!=NULL)
-            {
-                sum+=l2->val;
-                l2=l2->next;
-            }
+        if(carry!=0) {
+            newNode = new ListNode(carry);
+            output->next = newNode;
 
-            sum+=carry;
-            carry=sum/10;
-
-            ListNode* node=new ListNode(sum%10);
-            temp->next=node;
-            temp=temp->next;
         }
 
         return dummy->next;
-        
+
     }
+
 };
