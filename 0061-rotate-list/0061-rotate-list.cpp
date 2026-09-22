9334f41a526bd2b980dae34a01e3e7b991c3d785
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2026-09-22 20:12:14 +0530

    Time: 0 ms (100%), Space: 16.4 MB (63.98%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,69 +10,34 @@
  */
 class Solution {
 public:
-    int countNumOfNodes(ListNode* head){
-        ListNode* temp = head;
-        int count = 0;
-        while(temp!=nullptr){
-            count++;
-            temp=temp->next;
-        }
-        return count;
-
-    }
-    ListNode* reverse(ListNode* head){
-        ListNode* next = nullptr;
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-
-        while(curr!=nullptr){
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-        return prev;
-    }
+   
     ListNode* rotateRight(ListNode* head, int k) {
        
         if(head==nullptr)
             return nullptr;
 
-        int count = countNumOfNodes(head);
+        ListNode* temp = head;
+
+        int count = 1;
+
+        while(temp->next!=nullptr){
+            temp = temp->next;
+            count++;
+        }
+
         k = k%count;
-        if(k==0 || head->next == nullptr){
-            return head;
+
+        int n = count - k;
+
+        for(int i =0; i<n;i++){
+            temp->next = head ;
+            head = head->next;
+            temp = temp->next;
+            temp->next = nullptr;
         }
-        ListNode* temp = reverse(head);
+
+        return head;
+
         
-
-        ListNode* firstHalf = new ListNode(0);
-        ListNode* refFirstHalf= firstHalf;
-        ListNode* secondHalf = new ListNode(0);
-        while(temp!=nullptr){
-            if(k)
-                firstHalf->next= temp;
-            else {
-                firstHalf->next = nullptr;
-                secondHalf->next = temp;
-                break;
-            }
-               
-            temp=temp->next;
-            firstHalf=firstHalf->next;
-            k--;
-        }
-
-        ListNode* reverseFirstHalf = reverse(refFirstHalf->next);
-        ListNode* reverseSecondHalf = reverse(secondHalf->next);   
-
-        ListNode* temp1 = reverseFirstHalf;
-    
-        while(temp1->next!=nullptr){
-            temp1=temp1->next;   
-        }
-        temp1->next = reverseSecondHalf;
-        return reverseFirstHalf;
     }
 };
