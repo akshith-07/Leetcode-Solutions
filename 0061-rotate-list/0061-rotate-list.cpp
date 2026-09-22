561d182e1b590625ffc6b480cc78060b8604c719
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2026-09-22 10:56:09 +0530

    Time: 0 ms (100%), Space: 16.4 MB (31.84%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,39 +8,71 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-
 class Solution {
 public:
+    int countNumOfNodes(ListNode* head){
+        ListNode* temp = head;
+        int count = 0;
+        while(temp!=nullptr){
+            count++;
+            temp=temp->next;
+        }
+        return count;
+
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode* next = nullptr;
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while(curr!=nullptr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
     ListNode* rotateRight(ListNode* head, int k) {
-        if (!head || !head->next || k == 0) return head;
+       
+        if(head==nullptr)
+            return nullptr;
 
-        //find length and last node
-        int n = 1;
-        ListNode* tail = head;
-        while (tail->next) {
-            tail = tail->next;
-            n++;
+        int count = countNumOfNodes(head);
+        k = k%count;
+        if(k==0 || head->next == nullptr){
+            return head;
+        }
+        ListNode* temp = reverse(head);
+        
+
+        ListNode* firstHalf = new ListNode(0);
+        ListNode* refFirstHalf= firstHalf;
+        ListNode* secondHalf = new ListNode(0);
+        while(temp!=nullptr){
+            if(k)
+                firstHalf->next= temp;
+            else {
+                firstHalf->next = nullptr;
+                secondHalf->next = temp;
+                break;
+            }
+               
+            temp=temp->next;
+            firstHalf=firstHalf->next;
+            k--;
         }
 
-        // reduce k
-        k = k % n;
-        if (k == 0) return head;
+        ListNode* reverseFirstHalf = reverse(refFirstHalf->next);
+        ListNode* reverseSecondHalf = reverse(secondHalf->next);   
 
-        // make circular
-        tail->next = head;
-
-        // find new tail (n - k - 1 steps)
-        int steps = n - k;
-        ListNode* newTail = head;
-        while (--steps) {
-            newTail = newTail->next;
+        ListNode* temp1 = reverseFirstHalf;
+    
+        while(temp1->next!=nullptr){
+            temp1=temp1->next;   
         }
-
-        // break circle
-        ListNode* newHead = newTail->next;
-        newTail->next = nullptr;
-
-        return newHead;
+        temp1->next = reverseSecondHalf;
+        return reverseFirstHalf;
     }
 };
